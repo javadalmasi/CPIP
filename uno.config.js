d63c09616f055f47c41868b49da0b70ec45dbb18
javadalmasi <javadalmasi@outlook.com>
2025-03-23 00:30:05 +0330

    add vazirmatn

--- uno.config.js
+++ uno.config.js
@@ -19,22 +19,9 @@ export default defineConfig({
             },
         }),
         presetWebFonts({
-            provider: "none",
+            provider: "google",
             fonts: {
-                sans: [
-                    "-apple-system",
-                    "BlinkMacSystemFont",
-                    "Segoe UI",
-                    "Roboto",
-                    "Helvetica Neue",
-                    "Arial",
-                    "Noto Sans",
-                    "sans-serif",
-                    "Apple Color Emoji",
-                    "Segoe UI Emoji",
-                    "Segoe UI Symbol",
-                    "Noto Color Emoji",
-                ],
+                sans: 'Vazirmatn'
             },
         }),
     ],
